6fbbcb41fe4b197d6b6b8d281f1d4564d5a134ec
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-05-02 12:41:26 -0700

    added public folder, css and js files, and commenting to layout code required

---
+++ public/js/profile.js
@@ -0,0 +1,38 @@
+//new poll handler
+
+//get values from new poll form
+
+//const question, value trim
+
+//const option1, value trim
+
+//const option2, value trim
+
+
+//if question, option1, and option2 are not empty, and response is ok, send alert saying
+// poll created and send to profile
+
+
+
+
+//edit poll handler
+
+//get values from edit poll form
+
+//const question, value trim
+
+//const option1, value trim
+
+//const option2, value trim
+
+//if question and option are not empty and respose ok, send alert poll updated, send to profile
+
+
+//delete poll handler
+
+//prompt, 'are you sure you want to delete this poll?'
+
+//if yes, delete poll, send alert poll deleted, send to profile
+
+//if no, send alert poll not deleted, send to profile
+
