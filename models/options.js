0054b0e59f1c6ac360d99aa5b907b3f56d066648
spamdalfz <coreyvasser@gmail.com>
2023-05-01 20:46:53 -0700

    added model files for db. user model done

---
+++ models/options.js
@@ -0,0 +1,1 @@
+// options model here
---
+++ models/user.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
