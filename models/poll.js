55e12773725ff3a8c7cff979bd68ed5241e6a1f4
spamdalfz <coreyvasser@gmail.com>
2023-05-02 21:24:50 -0700

    defined poll model

--- models/poll.js
+++ models/poll.js
@@ -1,1 +1,40 @@
-// polls model here
+class Poll extends Model { }
+
+Poll.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'poll'
+    }
+);
+
+module.exports = Poll;
