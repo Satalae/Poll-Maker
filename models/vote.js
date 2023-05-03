ad2aef88f51df9acc71d4c6cfb233679a7883ebf
spamdalfz <coreyvasser@gmail.com>
2023-05-02 21:07:38 -0700

    defined vote model

--- models/vote.js
+++ models/vote.js
@@ -1,1 +1,40 @@
 // vote model here
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model { }
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        option_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'option',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
