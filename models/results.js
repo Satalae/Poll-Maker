4f92b8637977aec06898f2c7108383665d59d7f8
spamdalfz <coreyvasser@gmail.com>
2023-05-02 21:21:45 -0700

    defined results model

--- models/results.js
+++ models/results.js
@@ -1,1 +1,43 @@
-// results model here
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Poll extends Model { }
+
+Poll.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'poll'
+    }
+);
+
+module.exports = Poll;
