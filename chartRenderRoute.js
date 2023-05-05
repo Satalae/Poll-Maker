dfb7fc9ec488dd1dece5011dbd51305fbe83a21c
spamdalfz <coreyvasser@gmail.com>
2023-05-04 19:55:50 -0700

    changed query to 1 poll rather than all

--- chartRenderRoute.js
+++ chartRenderRoute.js
@@ -1,20 +1,22 @@
 const router = require('express').Router();
 const { Poll, Result } = require('./models');
 
-// get poll results 
-router.get('/', async (req, res) => {
+// get poll results for a single poll
+router.get('/:id', async (req, res) => {
     try {
-        const polls = await Poll.findAll({
-            include: [
-                {
-                    model: Result,
-                    attributes: ['option_1_votes', 'option_2_votes']
-                }
-            ]
+        const poll = await Poll.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: Result,
+                attributes: ['option_1_votes', 'option_2_votes']
+            }]
         });
-
-        // Render the poll results using Handlebars.js
-        res.render('polls', { polls });
+        // Render the poll results for a single poll using Handlebars.js
+        res.render('polls', {
+            polls: [poll]
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
