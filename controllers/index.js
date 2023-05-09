ed4fc3596c043015bc13780dd0a22a1f5b236b17
spamdalfz <coreyvasser@gmail.com>
2023-05-09 00:35:50 -0700

    added chart routes to controller/index

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const chartView = require('./chartRenderRoute')
 const { createVote } = require('./voteController');
 
+router.use('/chart', chartView)
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.post('/votes', createVote);
