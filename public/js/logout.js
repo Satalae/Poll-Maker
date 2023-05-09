0747d47edff97775d4febd04d36f3d10e000026b
Satalae <harrison19988@gmail.com>
2023-05-09 10:58:14 -0700

    login/signup/logout functional

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,16 @@
 // logout handler
 const logoutFormHandler = async (event) => {
     event.preventDefault();
-    const response = await fetch('/api/users',{
+    const response = await fetch('/api/users/logout',{
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
     });
-    if (response.status === 200) {
+    if (response.ok) {
+        console.log("logout successful!");
         document.location.replace('/');
         } else {
             alert('failed to logout');
     }
 };
 
-//document.querySelector('#logout').addEventListener('click', logoutFormHandler);
+document.querySelector('.logout').addEventListener('click', logoutFormHandler);
